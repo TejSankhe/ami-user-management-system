cbb19ae041a9c955289821cabfceb60b7a9cc134
sankhet <sankhe.t@northeastern.edu>
2020-02-17 14:17:22 -0800

    ci/cd job

--- scripts/install-java.sh
+++ scripts/install-java.sh
@@ -5,3 +5,6 @@ sudo apt-get update
 sudo apt install openjdk-8-jdk
 java -version
 echo 'installed java successfully.'
+echo 'installing tomcat'
+sudo apt-get install tomcat8 tomcat8-docs tomcat8-examples tomcat8-admin apache2 -y
+echo 'installed tomcat successfully.'
