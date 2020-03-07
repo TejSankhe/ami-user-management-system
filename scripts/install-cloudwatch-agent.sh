1be7707a83113199935020b9ceb1fb0b4d2f2d29
TejSankhe <tejsankhe03@gmail.com>
2020-03-07 15:57:28 -0500

    added codedeploy installation

---
+++ scripts/install-cloudwatch-agent.sh
@@ -0,0 +1,10 @@
+sudo apt-get install ruby
+sudo apt-get install wget
+cd /home/ubuntu
+echo "Downloading codedeploy for us-east-1"
+wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+echo "codedeploy for us-east-1 downloaded successfully"
+chmod +x ./install
+sudo ./install auto
+echo "Checking whether codedeploy status"
+sudo service codedeploy-agent status
