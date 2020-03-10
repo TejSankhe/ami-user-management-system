e19a41a3b92a2674093118ab80fd1a34831a04ef
TejSankhe <tejsankhe03@gmail.com>
2020-03-10 18:30:44 -0400

    change branch to master

--- scripts/install-cloudwatch-agent.sh
+++
@@ -1,8 +0,0 @@
-sudo apt-get install ruby -y
-sudo apt-get install wget -y
-cd /home/ubuntu
-echo "Downloading codedeploy for us-east-1"
-wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
-echo "codedeploy for us-east-1 downloaded successfully"
-chmod +x ./install
-sudo ./install auto
